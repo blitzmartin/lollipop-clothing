20716d7e2ee7bddb70b62cb7c274ee0c2b94bfed
blitzmartin <lizmartinmusic@gmail.com>
2022-09-22 15:13:26 +0200

    first commit

---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+export default function Card () {
+    return(
+        <div className="category-container">
+        <img src="" alt="" />
+        <div className="category-body-container">
+          <h2>Jackets</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+    )
+}
